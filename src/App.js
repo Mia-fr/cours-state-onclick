dd1ca552b5c23436c08dd464d6ef937ca343edec
Mia <apple@MyriamMac.local>
2024-03-21 16:59:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
+  // Ces 4 states sont relié aux input correspondants
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+
+  // State qui gère l'affichage ou non d'un message d'erreur à côté de mon bouton submit
+  const [showError, setShowError] = useState(false);
+
+  // Fonction déclenchée lors de la soumission du formulaire
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (password.length > 8 && password === passwordConf) {
+      setShowError(false);
+      console.log("ok");
+      return;
+    } else {
+      setShowError(true);
+      return alert(
+        "Votre mot de passe doit faire plus de 8 caractères et identique dans les deux champs!"
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <h1>Create account</h1>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}
+      >
+        <p>Name</p>
+
+        <input
+          value={name}
+          type="text"
+          placeholder="Jean Dupont"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <p>Email</p>
+
+        <input
+          value={email}
+          type="email"
+          name="email"
+          placeholder="jeandupont@lereacteur.io"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+        <p>Password</p>
+
+        <input
+          value={password}
+          type="password"
+          name="password"
+          placeholder="Your Secret Password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        ></input>
+
+        <p>Confirm your password</p>
+
+        <input
+          value={passwordConf}
+          type="password"
+          name="password"
+          placeholder="Your Secret Password to confirm"
+          onChange={(event) => {
+            setPasswordConf(event.target.value);
+          }}
+        />
+        <br></br>
+        <button type="submit">Register</button>
+      </form>
+      {showError === true && (
+        <p style={{ color: "red" }}>
+          Votre mot de passe doit faire plus de 8 caractères
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      )}
+      <Footer />
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Made with <strong>React</strong> at <strong>Le Reateur</strong> by{" "}
+        <strong>Brice</strong>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
